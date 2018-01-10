76be1e7c2249b2e295cf809f40643c17cd0bb82f
luoxianjie <jiexianluo@hotmail.com>
2018-01-10 11:44:00 +0800

    将session存储到memcache中

--- SSO/cookie-shared/sessiona/index.php
+++ SSO/cookie-shared/sessiona/index.php
@@ -4,6 +4,8 @@
 // | Date  : 2018/1/9
 // | Time  : 18:04
 // +----------------------------------------------------------------------
+ini_set("session.save_handler", "memcache");
+ini_set("session.save_path", "tcp://127.0.0.1:11211");
 session_start();
 header('Set-Cookie:PHPSESSID='. session_id() .'; domain=.lxj.com');
 class A
