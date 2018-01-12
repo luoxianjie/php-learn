06752540808056f829bef22fd0585b42ce64a459
luoxianjie <jiexianluo@hotmail.com>
2018-01-12 15:11:14 +0800

    修改小bug

--- Project/fastphp/base/Model.php
+++ Project/fastphp/base/Model.php
@@ -17,7 +17,7 @@ class Model extends Sql
         if(!$this->table){
             $modelClass = get_class($this);
 
-            $this->model = substr($this->model,0,-5);
+            $this->model = substr($modelClass,0,-5);
 
             $this->table = strtolower($this->model);
         }
