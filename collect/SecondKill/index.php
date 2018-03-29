c151c6040da8c7139e415ae468c8d57c15321c43
luoxianjie <jiexianluo@hotmail.com>
2018-03-29 18:56:46 +0800

    完善秒杀demo

--- collect/SecondKill/index.php
+++ collect/SecondKill/index.php
@@ -1,9 +1,12 @@
 <?php
+
 // +----------------------------------------------------------------------
 // | Author: jiexianluo@hotmail.com
 // | Date  : 2018/3/29
 // | Time  : 17:59
 // +----------------------------------------------------------------------
+require './Db.php';
+
 
 class SecondKill
 {
@@ -19,7 +22,24 @@ class SecondKill
 
     public function buy()
     {
+        $redis = new Redis();
+        $redis->connect('127.0.0.1',6379);
+        $key = 'goods:1';
 
+        $store = $redis->get($key);
+
+        if($store>0) {
+
+            // 随机取出一个会员id
+            $userId = $redis->sPop('userId');
+
+            // 将会员id压入购买者队列
+            $redis->lPush('buyerId', $userId);
+
+            // 商品库存自减
+            $redis->decr($key);
+
+        }
     }
 
     public function __call($name, $arguments)
@@ -29,5 +49,23 @@ class SecondKill
 }
 
 $action = isset($_GET['act'])?trim($_GET['act']):'buy';
-
 $data = (new SecondKill())->$action();
+
+
+
+/*// 从数据库取出所有用户
+$db = Db::getInstance();
+
+$users = $db->table('user')->select();
+
+$redis = new Redis();
+$redis->connect('127.0.0.1',6379);
+$key = 'goods:1';
+
+foreach ($users as $user) {
+    $redis->sAdd('userId',$user['id']);
+}
+
+// 设置商品库存为20
+$redis->set($key,20);*/
+
